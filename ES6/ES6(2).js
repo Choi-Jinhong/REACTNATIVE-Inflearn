0ca873fb6152cca6c90d1ef6cd6357e012f9eb49
최진홍 <gnong@choejinhong-ui-MacBookPro.local>
2020-11-26 16:26:33 +0900

    String literal

---
+++ ES6/ES6(2).js
@@ -0,0 +1,13 @@
+// String literal
+
+const val01 = 'Hello';
+const val02 = 'World';
+const val03 = val01 + ' ' + val02 + ' !!!!';
+
+console.log(val03);
+
+// `${variable}`
+
+const val04 = `${val01} ${val02} !!!!`
+
+console.log(val04);
