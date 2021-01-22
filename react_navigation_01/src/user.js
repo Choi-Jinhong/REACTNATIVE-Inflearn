db8a7705306fac3e2be8785d216e27f468327be3
Gnong <jinhongdev@gmail.com>
2021-01-22 09:19:51 +0900

    React Navigation 소개 ~ [Stack] Navigation Parmas

---
+++ react_navigation_01/src/user.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+
+class UserScreen extends Component {
+    render() {
+
+        const { params } = this.props.route;
+        const userIdx = params ? params.userIdx : null;
+        const userName = params ? params.userName : null;
+        const userLastName = params ? params.userLastName : null;
+        
+        return (
+            <View style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Text>User Screen</Text>
+                <Button
+                    title="To Home Screen"
+                    onPress={() => {
+                        this.props.navigation.navigate('Home')
+                    }}
+                />
+                <Text>User Idx: {JSON.stringify(userIdx)}</Text>
+                <Text>User Name: {JSON.stringify(userName)}</Text>
+                <Text>User LastName: {JSON.stringify(userLastName)}</Text>
+            </View>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default UserScreen;
